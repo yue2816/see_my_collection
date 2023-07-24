2983b10ac32be6b84ba41f790cda93b18e5b7b67
EC2 Default User <ec2-user@ip-172-31-30-191.ap-northeast-1.compute.internal>
2023-07-24 21:32:06 +0000

    [Add]bootstrap、FontAwesome、ログイン、新規登録画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # 管理者側のdeviseのルーティング
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # ユーザー側のdeviseのルーティング
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # ユーザー側のルーティング設定
+  scope module: :public do
+    root to: 'homes#top'
+    get 'homes/about'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
