95218f4234dd859a7d9bc83980a1588c4632573b
EC2 Default User <ec2-user@ip-172-31-30-191.ap-northeast-1.compute.internal>
2023-09-10 00:29:19 +0000

    seedにテストデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,54 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create!(
-  email: "abcde@gmail.com",
-  password: "qwerty"
-)
+olivia = User.find_or_create_by!(email: "olivia@example.com") do |user|
+  user.name = "Olivia"
+  user.password = "password"
+  user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
+  user.introduction = "クマのぬいぐるみが大好き！"
+  user.my_collection = "ぬいぐるみ"
+end
+
+james = User.find_or_create_by!(email: "james@example.com") do |user|
+  user.name = "James"
+  user.password = "password"
+  user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
+  user.introduction = "ミニカーを集めるのが好きです。"
+  user.my_collection = "ミニカー"
+end
+
+lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
+  user.name = "Lucas"
+  user.password = "password"
+  user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
+  user.introduction = "切手集めが趣味です"
+  user.my_collection = "切手"
+end
+
+Post.find_or_create_by!(title: "赤いリボンのクマのぬいぐるみ") do |post|
+  post.post_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
+  post.body = "誕生日におばあちゃんから貰った大事なぬいぐるみ。"
+  post.user = olivia
+end
+
+Post.find_or_create_by!(title: "ボーダー服のクマのぬいぐるみ") do |post|
+  post.post_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg")
+  post.body = "旅行先で出会ったぬいぐるみ。"
+  post.user = olivia
+end
+
+Post.find_or_create_by!(title: "青いカラフルなバスのミニカー") do |post|
+  post.post_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg")
+  post.body = "友人と玩具屋に行った時に買ったもの。"
+  post.user = james
+end
+
+Post.find_or_create_by!(title: "綺麗な色合いの切手") do |post|
+  post.post_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post4.jpg"), filename:"sample-post4.jpg")
+  post.body = "海外に旅行に行った時に買った。"
+  post.user = lucas
+end
+
+Admin.find_or_create_by!(email: "admin@example.com") do |admin|
+  admin.password = ENV['ADMIN_PASSWORD']
+end
